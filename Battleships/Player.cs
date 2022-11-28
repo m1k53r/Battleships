6fe53a734b6de5e804740e16c1b9841a470cf82b
Gruzjan <gracjan122@protonmail.com>
2022-11-28 19:18:57 +0100

    MVP Game Logic & UI

---
+++ Battleships/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Ship> Ships { get; set; }
+        public List<Tile> Tiles { get; set; }
+        public Player(string name)
+        {
+            Name = name;
+            Tiles = new List<Tile>();
+            Ships = new List<Ship>();
+        }
+    }
+}
---
+++ Battleships/Ship.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    public class Ship
+    {
+        public string Name { get; }
+        public int Length { get; set; }
+        public List<Tile> Tiles{ get; set; }
+        public int ShipDir { get; } // 0 - up, 1 - right
+        public bool IsDestroyed { get; set; }
+        public Ship(int length, string name, int shipDir)
+        {
+            Length = length;
+            Name = name;
+            ShipDir = shipDir;
+            Tiles = new List<Tile>();
+        }
+    }
+}
